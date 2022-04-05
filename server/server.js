1ed0888c6592a8a6ee649b174b2a18fb70e9372b
nick0726 <75522130+nick0726@users.noreply.github.com>
2022-04-05 19:13:47 +0900

    Update cors origin

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const dotenv = require("dotenv").config();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || "3000";
 const path = require("path");
 const cors = require("cors");
 const mongoose = require("mongoose");
