d087ca5164792afcb34c1987907f1f0d93cd2c83
Nam <75522130+nick0726@users.noreply.github.com>
2022-04-05 17:22:41 +0900

    Upload server

---
+++ server/server.js
@@ -0,0 +1,166 @@
+const express = require('express');
+const app = express();
+const dotenv = require('dotenv').config();
+const port = process.env.PORT || 3000;
+const path = require('path');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const User = require('./models/user.models');
+const jwt = require('jsonwebtoken');
+
+// const generateToken = (id) => {
+//   return jwt.sign({ id }, process.env.JWT_SCRET, {
+//     expiresIn: '30d',
+//   });
+// };
+
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect(
+  'mongodb+srv://nick0726:qweR1234@cluster0.yzv2q.mongodb.net/neuroears?retryWrites=true&w=majority',
+  { useNewUrlParser: true }
+);
+
+const http = require('http').createServer(app);
+http.listen(port, () => {
+  console.log(`${port}에 포트열림 서버 오픈 완료`);
+});
+
+app.use('/', express.static(path.join(__dirname, '../client/build')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
+
+app.post('/signup', async (req, res) => {
+  console.log(req.body);
+  try {
+    await User.create({
+      id: req.body.id,
+      password: req.body.password,
+      name: req.body.name,
+      email: req.body.email,
+      // token: generateToken(req.body.id),
+    });
+    // var temp = User.create();
+    // console.log(temp);
+    res.json({ status: 'ok' });
+  } catch (err) {
+    // console.log(err);
+    res.json({ status: 'error', error: 'Duplicate id or email' });
+  }
+});
+
+app.post('/login', async (req, res) => {
+  const user = await User.findOne({
+    id: req.body.id,
+    password: req.body.password,
+    // token: generateToken(req.body.id),
+  });
+
+  if (user) {
+    const token = jwt.sign(
+      {
+        id: req.body.id,
+        email: req.body.email,
+        // token: generateToken(user.id),
+      },
+      `scret${dotenv.JWT_SCRET}`
+    );
+    return res.json({
+      status: 'ok',
+      user: token,
+    });
+  } else {
+    return res.json({
+      status: 'error',
+      user: false,
+    });
+  }
+});
+
+app.get('/login', async (req, res) => {
+  const token = req.headers['x-acces-token'];
+
+  try {
+    const decoded = jwt.verify(token, `scret${dotenv.JWT_SCRET}`);
+    const id = decoded.id;
+    // const user = await User.findOne({ id: id });
+    return { status: 'ok' /*quote: user.quote */ };
+  } catch (error) {
+    console.log(error);
+    res.json({ status: 'error', error: 'invalid token' });
+  }
+
+  const user = await User.findOne({
+    id: req.body.id,
+    password: req.body.password,
+    // token: generateToken(req.body.id),
+  });
+
+  if (user) {
+    const token = jwt.sign(
+      {
+        id: req.body.id,
+        email: req.body.email,
+        // token: generateToken(user.id),
+      },
+      `scret${dotenv.JWT_SCRET}`
+    );
+    return res.json({
+      status: 'ok',
+      user: token,
+    });
+  } else {
+    return res.json({
+      status: 'error',
+      user: false,
+    });
+  }
+});
+
+app.get('/mainpage', async (req, res) => {
+  const user = await User.findOne({
+    id: req.body.id,
+    password: req.body.password,
+    // token: generateToken(req.body.id),
+  });
+
+  if (user) {
+    const token = jwt.sign(
+      {
+        id: req.body.id,
+        email: req.body.email,
+        // token: generateToken(user.id),
+      },
+      `scret${dotenv.JWT_SCRET}`
+    );
+    return res.json({
+      status: 'ok',
+      user: token,
+    });
+  } else {
+    return res.json({
+      status: 'error',
+      user: false,
+    });
+  }
+});
+
+app.post('/mainpage', async (req, res) => {
+  const token = req.headers['x-acces-token'];
+
+  try {
+    const decoded = jwt.verify(token, `scret${dotenv.JWT_SCRET}`);
+    const id = decoded.id;
+    const user = await User.updateOne(
+      { id: id }
+      // { $set: { quote: req.body.quote } }
+    );
+    return { status: 'ok' };
+  } catch (error) {
+    console.log(error);
+    res.json({ status: 'error', error: 'invalid token' });
+  }
+});
