cbc8eb80a5a3dc76a92ee9a6e363fcc1aad22a64
nick0726 <75522130+nick0726@users.noreply.github.com>
2022-04-06 10:08:41 +0900

    Update MainPage in client and add cors in server.js

--- client/src/pages/Main.js
+++ client/src/pages/Main.js
@@ -1,16 +1,16 @@
-import React, { useEffect } from 'react';
-import LandingImage from '../images/landingimage.jpg';
-import { Link, useHistory } from 'react-router-dom';
-import '../Main.css';
-import jwt from 'jsonwebtoken';
+import React, { useEffect } from "react";
+import LandingImage from "../images/landingimage.jpg";
+import { Link, useHistory } from "react-router-dom";
+import "../Main.css";
+import jwt from "jsonwebtoken";
 // const jwt = require('jsonwebtoken');
 
 const MainPage = () => {
   const history = useHistory();
   async function populateQuote() {
-    const req = await fetch('http://localhost:3000/mainpage', {
+    const req = await fetch("http://localhost:3000/mainpage", {
       headers: {
-        'x-access-token': localStorage.getItem('token'),
+        "x-access-token": localStorage.getItem("token"),
       },
     });
     const data = req.json();
@@ -18,29 +18,30 @@ const MainPage = () => {
   }
 
   useEffect(() => {
-    const token = localStorage.getItem('token');
+    const token = localStorage.getItem("token");
     if (token) {
       const user = jwt.decode(token);
       if (!user) {
-        alert('Check the ID or password');
-        localStorage.removeItem('token');
-        history.replace('/login');
+        alert("Check the ID or password");
+        localStorage.removeItem("token");
+        history.replace("/login");
       } else {
         populateQuote();
       }
     }
   }, []);
+
   return (
     <>
       <div
-        className="container"
+        className='container'
         style={{ backgroundImage: `url(${LandingImage})` }}
       >
-        <div className="header">
-          <Link to="/mainpage">
+        <div className='header'>
+          <Link to='/mainpage'>
             <h1>NeuroEars</h1>
           </Link>
-          <div className="nav">
+          <div className='nav'>
             <ul>
               <li>
                 <p>About us</p>
@@ -61,14 +62,14 @@ const MainPage = () => {
           </div>
         </div>
 
-        <div className="mainIntro">
+        <div className='mainIntro'>
           <h2>We serve the best rehabilitation experiences</h2>
           <p>
             You can fix your dizziness, and can get free from vertigo.
             <br />
             Join with us to gain again the comfort dizziness free life agian.
           </p>
-          <button className="getStarted">Get Started</button>
+          <button className='getStarted'>Get Started</button>
         </div>
       </div>
     </>
